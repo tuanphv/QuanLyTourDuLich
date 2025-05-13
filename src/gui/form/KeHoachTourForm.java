2037f61ddd0655b5d0b945c4f704f1cf027d63dc
Thuat3Tuoi <itdontsleep@gmail.com>
2025-05-13 14:40:09 +0700

    update keHoachTour

--- src/gui/form/KeHoachTourForm.java
+++ src/gui/form/KeHoachTourForm.java
@@ -1,7 +1,19 @@
 package gui.form;
 
+import bus.BuoiAnBUS;
+import bus.ChiTietKeHoachTourBUS;
+import bus.DiaDanhBUS;
+import bus.DiaDiemThamQuanBUS;
 import bus.KeHoachTourBUS;
+import bus.KhachSanBUS;
+import bus.KhachSanNghiNgoiBUS;
+import bus.NhaHangBUS;
+import bus.PhuongTienBUS;
+import dto.BuoiAnDTO;
+import dto.ChiTietKeHoachTourDTO;
+import dto.DiaDiemThamQuanDTO;
 import dto.KeHoachTourDTO;
+import dto.KhachSanNghiNgoiDTO;
 import gui.components.MyScrollBarUI;
 import gui.dialog.KeHoachTour;
 import interfaces.SearchHandler;
@@ -9,17 +21,30 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
 
 public class KeHoachTourForm extends javax.swing.JPanel {
+    KeHoachTourBUS keHoachTourBUS = new KeHoachTourBUS();
+    ChiTietKeHoachTourBUS chiTietKeHoachTourBUS = new ChiTietKeHoachTourBUS();
+    DiaDanhBUS diaDanhBUS = new DiaDanhBUS();
+    PhuongTienBUS phuongTienBUS = new PhuongTienBUS();
+    NhaHangBUS nhaHangBUS = new NhaHangBUS();
+    KhachSanBUS khachSanBUS = new KhachSanBUS();
+    DiaDiemThamQuanBUS diaDiemThamQuanBUS = new DiaDiemThamQuanBUS();
+    BuoiAnBUS buoiAnBUS = new BuoiAnBUS();
+    KhachSanNghiNgoiBUS khachSanNghiNgoiBUS = new KhachSanNghiNgoiBUS();
+    KeHoachTour keHoachTourDialog = new KeHoachTour(); // Tạo để sử dụng lại hàm
 
     public KeHoachTourForm() {
         initComponents();
+        
         spTable.getVerticalScrollBar().setUI(new MyScrollBarUI());
         spTable.getVerticalScrollBar().setPreferredSize(new Dimension(8, 0));
         spTable.getViewport().setBackground(Color.white);
@@ -58,6 +83,12 @@ public class KeHoachTourForm extends javax.swing.JPanel {
             }
         });
 
+        panelLichTrinh.setLayout(new BoxLayout(panelLichTrinh, BoxLayout.Y_AXIS));
+        panelAnUong.setLayout(new BoxLayout(panelAnUong, BoxLayout.Y_AXIS));
+
+        lbLichTrinh.setVisible(false);
+        lbAnUong.setVisible(false);
+
     }
 
     @SuppressWarnings("unchecked")
@@ -69,8 +100,14 @@ public class KeHoachTourForm extends javax.swing.JPanel {
         table = new gui.components.Table();
         jLabel1 = new javax.swing.JLabel();
         myToolBar1 = new gui.components.MyToolBar();
-
-        panelBorder1.setBackground(new java.awt.Color(255, 255, 255));
+        panelBorder3 = new gui.components.PanelBorder();
+        jLabel9 = new javax.swing.JLabel();
+        scrollChiTietKeHoachTour = new javax.swing.JScrollPane();
+        panelChiTietKeHoachTour = new gui.components.PanelBorder();
+        panelLichTrinh = new gui.components.PanelBorder();
+        panelAnUong = new gui.components.PanelBorder();
+        lbAnUong = new javax.swing.JLabel();
+        lbLichTrinh = new javax.swing.JLabel();
 
         spTable.setBorder(null);
 
@@ -82,6 +119,11 @@ public class KeHoachTourForm extends javax.swing.JPanel {
 
             }
         ));
+        table.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tableMouseClicked(evt);
+            }
+        });
         spTable.setViewportView(table);
 
         jLabel1.setBackground(new java.awt.Color(255, 255, 255));
@@ -110,6 +152,101 @@ public class KeHoachTourForm extends javax.swing.JPanel {
                 .addContainerGap(20, Short.MAX_VALUE))
         );
 
+        jLabel9.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel9.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(102, 102, 102));
+        jLabel9.setText("Chi tiết Kế hoạch Tour");
+
+        scrollChiTietKeHoachTour.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout panelLichTrinhLayout = new javax.swing.GroupLayout(panelLichTrinh);
+        panelLichTrinh.setLayout(panelLichTrinhLayout);
+        panelLichTrinhLayout.setHorizontalGroup(
+            panelLichTrinhLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        panelLichTrinhLayout.setVerticalGroup(
+            panelLichTrinhLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 382, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout panelAnUongLayout = new javax.swing.GroupLayout(panelAnUong);
+        panelAnUong.setLayout(panelAnUongLayout);
+        panelAnUongLayout.setHorizontalGroup(
+            panelAnUongLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 566, Short.MAX_VALUE)
+        );
+        panelAnUongLayout.setVerticalGroup(
+            panelAnUongLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 387, Short.MAX_VALUE)
+        );
+
+        lbAnUong.setBackground(new java.awt.Color(255, 255, 255));
+        lbAnUong.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lbAnUong.setForeground(new java.awt.Color(102, 102, 102));
+        lbAnUong.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        lbAnUong.setText("Ăn uống");
+
+        lbLichTrinh.setBackground(new java.awt.Color(255, 255, 255));
+        lbLichTrinh.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        lbLichTrinh.setForeground(new java.awt.Color(102, 102, 102));
+        lbLichTrinh.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        lbLichTrinh.setText("Lịch trình");
+
+        javax.swing.GroupLayout panelChiTietKeHoachTourLayout = new javax.swing.GroupLayout(panelChiTietKeHoachTour);
+        panelChiTietKeHoachTour.setLayout(panelChiTietKeHoachTourLayout);
+        panelChiTietKeHoachTourLayout.setHorizontalGroup(
+            panelChiTietKeHoachTourLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelChiTietKeHoachTourLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(panelChiTietKeHoachTourLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lbAnUong, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(panelLichTrinh, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(panelChiTietKeHoachTourLayout.createSequentialGroup()
+                        .addGroup(panelChiTietKeHoachTourLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lbLichTrinh, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(panelAnUong, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addContainerGap())))
+        );
+        panelChiTietKeHoachTourLayout.setVerticalGroup(
+            panelChiTietKeHoachTourLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelChiTietKeHoachTourLayout.createSequentialGroup()
+                .addGap(7, 7, 7)
+                .addComponent(lbLichTrinh, javax.swing.GroupLayout.DEFAULT_SIZE, 28, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelLichTrinh, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(lbAnUong, javax.swing.GroupLayout.DEFAULT_SIZE, 28, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelAnUong, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        scrollChiTietKeHoachTour.setViewportView(panelChiTietKeHoachTour);
+
+        javax.swing.GroupLayout panelBorder3Layout = new javax.swing.GroupLayout(panelBorder3);
+        panelBorder3.setLayout(panelBorder3Layout);
+        panelBorder3Layout.setHorizontalGroup(
+            panelBorder3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBorder3Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(panelBorder3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panelBorder3Layout.createSequentialGroup()
+                        .addComponent(jLabel9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGap(375, 375, 375))
+                    .addComponent(scrollChiTietKeHoachTour))
+                .addGap(20, 20, 20))
+        );
+        panelBorder3Layout.setVerticalGroup(
+            panelBorder3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelBorder3Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(scrollChiTietKeHoachTour, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                .addGap(20, 20, 20))
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -119,18 +256,82 @@ public class KeHoachTourForm extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(myToolBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(panelBorder1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(640, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(panelBorder3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(15, 15, 15)
-                .addComponent(myToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(15, 15, 15)
-                .addComponent(panelBorder1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(20, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(panelBorder3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(myToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(15, 15, 15)
+                        .addComponent(panelBorder1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(25, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void tableMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tableMouseClicked
+        int rowSelected = table.getSelectedRow();
+        int maKeHoachTour = (int) table.getModel().getValueAt(rowSelected, 0);
+        KeHoachTourDTO keHoachTour = keHoachTourBUS.getKeHoachTourById(maKeHoachTour);
+
+       
+
+        panelLichTrinh.removeAll();
+        panelAnUong.removeAll();
+
+        // lấy thông tin chi tiết kế hoạch tour
+        ArrayList<ChiTietKeHoachTourDTO> chiTietKeHoachTour = chiTietKeHoachTourBUS.getChiTietKeHoachTourByMaKeHoachTour(maKeHoachTour);
+        int soNgayTour = chiTietKeHoachTour.size();
+
+        for (int i = 0; i < soNgayTour; i++) {
+            ChiTietKeHoachTourDTO chiTietKeHoach = chiTietKeHoachTour.get(i);
+            int maChiTietKeHoach = chiTietKeHoach.getMaChiTietKeHoachTour();
+
+            JPanel lichTrinhPanel = keHoachTourDialog.createLichTrinhPanel(i + 1);
+            JPanel anUongPanel = keHoachTourDialog.createAnUongPanel(i + 1);
+
+            // Lấy thông tin lịch trình và ăn uống từ cơ sở dữ liệu
+            ArrayList<DiaDiemThamQuanDTO> listDiaDiemThamQuan = diaDiemThamQuanBUS.getDiaDiemThamQuanByMaChiTietKeHoachTour(maChiTietKeHoach);
+            ArrayList<KhachSanNghiNgoiDTO> listKhachSanNghiNgoi = khachSanNghiNgoiBUS.getKhachSanNghiNgoiByMaChiTietKeHoachTour(maChiTietKeHoach);
+            ArrayList<BuoiAnDTO> listBuoiAn = buoiAnBUS.getBuoiAnByMaChiTietKeHoachTour(maChiTietKeHoach);
+
+            // Thêm tiêu đề
+            JTextField titleField = (JTextField) ((JPanel) lichTrinhPanel.getComponent(0)).getComponent(0);
+            titleField.setText(chiTietKeHoach.getMoTa());
+
+            // Thêm địa điểm tham quan
+            JPanel diaDiemPanel = (JPanel) lichTrinhPanel.getComponent(1);
+            keHoachTourDialog.loadDataFormDiaDiem(diaDiemPanel, listDiaDiemThamQuan);
+
+            // Thêm khách sạn nghỉ ngơi
+            JPanel khachSanPanel = (JPanel) lichTrinhPanel.getComponent(2);
+            keHoachTourDialog.loadDataFormKhachSan(khachSanPanel, listKhachSanNghiNgoi);
+
+            // Thêm ăn uống
+            keHoachTourDialog.loadDataFormAnUong(anUongPanel, listBuoiAn);
+
+            // // Thêm panel lịch trình và ăn uống vào panel chính
+            panelLichTrinh.add(lichTrinhPanel);
+            panelAnUong.add(anUongPanel);
+        }
+
+        if (panelLichTrinh.getComponentCount() != 0) lbLichTrinh.setVisible(true);
+        else lbLichTrinh.setVisible(false);
+        if (panelAnUong.getComponentCount() != 0) lbAnUong.setVisible(true);
+        else lbAnUong.setVisible(false);
+        
+        // Cập nhật lại giao diện
+        panelLichTrinh.revalidate();
+        panelLichTrinh.repaint();
+        panelAnUong.revalidate();
+        panelAnUong.repaint();
+
+    }//GEN-LAST:event_tableMouseClicked
 
     private void btnThemActionPerformed(ActionEvent evt) {
         JFrame parentFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
@@ -247,9 +448,18 @@ public class KeHoachTourForm extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JLabel lbAnUong;
+    private javax.swing.JLabel lbLichTrinh;
     private gui.components.MyToolBar myToolBar1;
+    private gui.components.PanelBorder panelAnUong;
     private gui.components.PanelBorder panelBorder1;
+    private gui.components.PanelBorder panelBorder3;
+    private gui.components.PanelBorder panelChiTietKeHoachTour;
+    private gui.components.PanelBorder panelLichTrinh;
+    private javax.swing.JScrollPane scrollChiTietKeHoachTour;
     private javax.swing.JScrollPane spTable;
     private gui.components.Table table;
     // End of variables declaration//GEN-END:variables
 }
+
